5d019c32272b71edf260deca1c5abb990dc9c17c
Cooper Labinger <clabinger@gmail.com>
2016-10-25 21:52:26 +0000

    Deployment script: fix permissions at start

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(sudo echo "") && (composer install --no-dev --optimize-autoloader) && (php bin/console cache:clear --env=prod --no-debug) && (php bin/console assetic:dump --env=prod --no-debug) && (sudo chown -R root /srv; sudo chgrp -R web /srv; sudo chmod -R 770 /srv;)
+(sudo chown -R root /srv; sudo chgrp -R web /srv; sudo chmod -R 770 /srv;) && (composer install --no-dev --optimize-autoloader) && (php bin/console cache:clear --env=prod --no-debug) && (php bin/console assetic:dump --env=prod --no-debug) && (sudo chown -R root /srv; sudo chgrp -R web /srv; sudo chmod -R 770 /srv;)
