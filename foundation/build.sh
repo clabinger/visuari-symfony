56e2666745fdc2b67da0ef4f21047ec2865ca2ab
Cooper Labinger <clabinger@gmail.com>
2016-10-25 23:44:18 +0000

    Add fix permissions to foundation build script

--- foundation/build.sh
+++ foundation/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 target=../web/assets/vendor/foundation/
 rm -r $target/* 2> /dev/null
-cp _custom_settings.scss vendor/scss/. && cd vendor && sudo npm run build && cp -r {js,css} ../$target && echo "Successfully built libraries."
+cp _custom_settings.scss vendor/scss/. && cd vendor && sudo npm run build && cp -r {js,css} ../$target && (sudo chown -R root /srv; sudo chgrp -R web /srv; sudo chmod -R 770 /srv;) && echo "Successfully built libraries."
